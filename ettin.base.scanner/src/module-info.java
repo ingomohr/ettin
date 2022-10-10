439e72a1d4032f1b3efbe66533f05d71e33a6e39
Ingo Mohr <tellastory73@gmail.com>
2022-10-10 10:15:01 +0200

    feat: add scanner module

---
+++ ettin.base.scanner/src/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * Module to provide an API for scanners.
+ * 
+ * @author ingomohr
+ */
+module ettin.base.scanner {
+
+	requires java.base;
+	requires transitive ettin.base.model;
+	
+	exports org.ingomohr.ettin.base.scanner;
+}
